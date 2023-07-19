b7605040c9d2b73045c872f6fc24e699dbfac053
leeyang19970916 <yang19970916@gmail.com>
2023-07-19 15:07:54 +0800

    pagination complete

--- reservation/js/class/PAGINATION.js
+++ reservation/js/class/PAGINATION.js
@@ -1,64 +1,73 @@
 class PAGINATION {
-    constructor(totalPage) {
-      (this.dom = document.querySelector("#pagination")),
-        (this.totalPage = totalPage),
-        (this.nowPage = 1),
-        this.uiRender();
-        // this.eventBinding()
-    }
-    eventBinding(){
-      // 取得上一頁和下一頁的按鈕元素
-      const prevButton = this.dom.querySelector(".prevIcon");
-      const nextButton = this.dom.querySelector(".nextIcon");
-  
-      // 綁定上一頁和下一頁的點擊事件處理函式
-      prevButton.addEventListener("click", this.pageChange.bind(this, "prev"));
-      nextButton.addEventListener("click", this.pageChange.bind(this, "next"));
-    }
-    prevUI(d) {
-      // let disabled=false
-      if (this.nowPage===1) {
-          // disabled=true
-          console.log("start")
-      }
-      console.log(d,"ddd prev")
-      return `<i class="fa-solid fa-chevron-left pagination_arrow prevIcon ${d==='disabled' ? 'disabled' :''}"></i>`
-    }
-    nextUI(d) {
-      // let disabled=false
-      // if (this.nowPage===this.totalPage) {
-      //     disabled=true
-      // }
-      console.log(d,"ddd next")
-      return `<i class="fa-solid fa-chevron-right pagination_arrow nextIcon ${d==='disabled' ? 'disabled' :''}"></i>`
-    }
-    pageChange(action) {
-      if (action === "prev") {
-        if (this.nowPage === 1) {
-
-        }else{
-          this.nowPage--;
-        }
-      } else {
-        if (this.nowPage === this.totalPage) {
-          this.nextUI("disabled");
-        }else{
-          this.nowPage++;
-        }
-      }
+  constructor(totalPage) {
+    (this.dom = document.querySelector("#pagination")),
+      (this.totalPage = totalPage),
+      (this.nowPage = 1),
       this.uiRender();
-    }
-    uiRender() {
-      let str = "";
-      let prevUI = this.prevUI();
-      let nextUI = this.nextUI();
-      for (let i = 1; i < this.totalPage + 1; i++) {
-        str += `<li class="page__numbers ${
-          this.nowPage === i ? "active" : ""
-        }">${i}</li> `;
-      }
-      this.dom.innerHTML = `${prevUI}${str} ${nextUI} `;
-      this.eventBinding()
-    }
+    // this.eventBinding()
   }
-  
+  changePage(page){
+    if (page===this.nowPage) {
+      return
+    }
+    this.nowPage=page
+    this.uiRender();
+  }
+  eventBinding() {
+    // 取得上一頁和下一頁的按鈕元素
+    const prevButton = this.dom.querySelector(".prevIcon");
+    const nextButton = this.dom.querySelector(".nextIcon");
+
+    // 綁定上一頁和下一頁的點擊事件處理函式
+    prevButton.addEventListener("click", this.pageChange.bind(this, "prev"));
+    nextButton.addEventListener("click", this.pageChange.bind(this, "next"));
+  }
+  prevUI(state) {
+    return `<i class="fa-solid fa-chevron-left pagination_arrow prevIcon ${state===1 ?'disabled' : ""}"></i>`;
+  }
+  nextUI(state) {
+    return `<i class="fa-solid fa-chevron-right pagination_arrow nextIcon  ${state==='max' ?'disabled' : ""} "></i>`;
+  }
+  pageChange(action) {
+    if (action === "prev") {
+      if (this.nowPage === 1) {
+        return;
+      } else {
+        this.nowPage--;
+      }
+    } else {
+      if (this.nowPage === this.totalPage) {
+        return;
+      } else {
+        this.nowPage++;
+      }
+    }
+    this.uiRender();
+  }
+  uiRender() {
+    let str = "";
+    let prevUI=''
+    let nextUI=''
+    if (this.nowPage===1) {
+      prevUI=this.prevUI(1);
+    }else{
+      prevUI=this.prevUI();
+    }
+
+    if (this.nowPage===this.totalPage) {
+      nextUI=this.nextUI('max');
+    }else{
+      nextUI=this.nextUI();
+    }
+    for (let i = 1; i < this.totalPage + 1; i++) {
+      str += `<li onclick="selectPage(${i})" class="page__numbers ${
+        this.nowPage === i ? "active" : ""
+      }">${i}</li> `;
+    }
+    this.dom.innerHTML = `${prevUI}${str} ${nextUI} `;
+    this.eventBinding();
+  }
+}
+function selectPage(p) {
+  page.changePage(p)
+}
