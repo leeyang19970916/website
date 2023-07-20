85f88a9fe63e63c147c99343031c306bc4fe4930
leeyang19970916 <yang19970916@gmail.com>
2023-07-20 17:50:21 +0800

    8549

--- reservation/js/error_message.js
+++ reservation/js/error_message.js
@@ -1,4 +1,10 @@
-function error_message(dom) {
+function error_message(dom,state="show") {
+  if (state==="hide") {
+    dom.classList.add("d-none")
+    return
+  }else{
+    dom.classList.remove("d-none")
+  }
     let image=`<img class='icon' src='/reservation/image/icon/notice_icon.svg'>`
     let str=`輸入資料格式或內容錯誤，請重新輸入`
   dom.innerHTML=`${image}${str}`
